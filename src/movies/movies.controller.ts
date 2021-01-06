a6743a638bf0b78716dc2baba074e8d4f69bcba1
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-01-07 07:33:14 +0900

    More Controller(add Decorator: @Param, @Query)

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 import { getOuterBindingIdentifiers } from '../../node_modules/@babel/types';
 import { create } from 'domain';
 
@@ -9,15 +9,19 @@ export class MoviesController {
     getAll() {
         return "this will return all movies"
     }
-
+    @Get('search')
+    search(@Query("year") searchingYear:string) {
+        return `We are searching for a movie made after: ${searchingYear}`;
+    }
+    
     @Get("/:id")
     getOne(@Param("id") movieId:string)  {
         return `This will return one movie id: ${movieId}`;
     }
-
+    
     @Post()
-    create() {
-        return `This will create a movie`;
+    create(@Body() movieData) {
+        return movieData;
     }
     @Delete("/:id") 
     remove(@Param("id") movieId:string) {
@@ -25,9 +29,13 @@ export class MoviesController {
     }
 
     @Patch('/:id')
-    Patch(@Param('id') movieId: string) {
-        return `This will patch a movie with the id: ${movieId}`;
+    Patch(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updateMoive: movieId,
+            ...updateData
+        };
     }
+    // '/search'가 아닌 'search' 로 사용가능 
     
 
 }
